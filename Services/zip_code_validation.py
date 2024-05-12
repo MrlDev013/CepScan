6d2047f824ed5e8e0b70117491b6b2cbab88a5c7
MrlDev013 <murilolives20@gmail.com>
2024-05-12 14:13:06 -0300

    Refactoring variables and adding new method.

--- Services/zip_code_validation.py
+++ Services/zip_code_validation.py
@@ -1,18 +1,21 @@
 import requests
+import Services.sheet_add_column_result as scr
 
 def zip_code_validation(sheet):
 
     column_name = 'CEP'
     column = sheet[column_name]
 
-    for indice, valor in column.items():
-        url = f'https://viacep.com.br/ws/{valor}/json/'
+    for index, value in column.items():
+        url = f'https://viacep.com.br/ws/{value}/json/'
         response = requests.get(url)
+        data = None
 
         if response.status_code == 200:
             data = response.json()
+            scr.create_sheet_column_result(sheet, data)
             print(data)
         else:
-            print(f"Erro na requisição:{response.status_code} | Valor: {valor}")
+            print(f"Erro na requisição:{response.status_code} | Resultado: {value}")
 
-    
+    return sheet
